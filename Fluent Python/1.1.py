bf8f3784519be3bed53f20afbeda67a60f3cf0e1
liudehan <67781193@qq.com>
2019-05-16 09:25:22 +0800

    代码提交

--- Fluent Python/1.1.py
+++ Fluent Python/1.1.py
@@ -15,40 +15,40 @@ class FrenchDeck:
     def __getitem__(self, position):
         return self._cards[position]
 
-# if __name__ == '__main__':
-#     frenchDeck = FrenchDeck()
-#     for card in frenchDeck:
-#         print(card)
-#
-#
-#     for card in reversed(frenchDeck):
-#         print(card)
+if __name__ == '__main__':
+    frenchDeck = FrenchDeck()
+    for card in frenchDeck:
+        print(card)
 
-# beer_card = Card('7', 'diamonds')
-# print(beer_card)
-#
+
+    for card in reversed(frenchDeck):
+        print(card)
+
+beer_card = Card('7', 'diamonds')
+print(beer_card)
+
 deck = FrenchDeck()
-# print(len(deck))
-# print(deck[0])
-# print(deck[-1])
-#
-# from random import choice
-# print(choice(deck))
-# print(choice(deck))
-# print(choice(deck))
-#
-# print(deck[:3])
-# print(deck[12::13])
+print(len(deck))
+print(deck[0])
+print(deck[-1])
+
+from random import choice
+print(choice(deck))
+print(choice(deck))
+print(choice(deck))
+
+print(deck[:3])
+print(deck[12::13])
 # 正向迭代
-# for card in deck:
-#     print(card)
+for card in deck:
+    print(card)
 
 # 反向迭代
-# for card in reversed(deck):
-#     print(card)
+for card in reversed(deck):
+    print(card)
 
-# print(Card('Q', 'hearts') in deck)
-# print(Card('7', 'beasts') in deck)
+print(Card('Q', 'hearts') in deck)
+print(Card('7', 'beasts') in deck)
 
 suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 def spades_high(card):
